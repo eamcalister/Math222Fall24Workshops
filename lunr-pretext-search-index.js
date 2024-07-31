957106c8a7d0184896c8ce14cd7da52073fa430c
eamcalister <mcalister_e@fortlewis.edu>
2024-07-31 17:30:43 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,167 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  copyright  "
+},
+{
+  "id": "Workshops-3",
+  "level": "1",
+  "url": "Workshops-3.html",
+  "type": "Worksheet",
+  "number": "1.1",
+  "title": "Integrals and Kinetic Energy",
+  "body": "Integrals and Kinetic Energy    In this workshop you will find the formula for the kinetic energy of an object of mass and velocity , then apply this formula to find the total kinetic energy of a spinning sphere.  The formula for kinetic energy First, we will establish the following notation for this problem:   Velocity = = The rate of change in position.    Acceleration = = the rate of change in velocity.    Force equals mass acceleration     Work = Force distance when the force is constant.   The key to finding a formula for kinetic energy is to observe that the kinetic energy of an object of mass and velocity is the work done in accelerating that object to velocity from rest. Assuming that velocity is achieved when , the work integral to calculate kinetic energy is where the velocity is reached after seconds.   (3 points) Explain how the integral given above gives the work done in accelerating an object of mass $m$ to a velocity .    (5 points) Evaluate the integral given above to compute the work done in accelerating an object of mass from rest (when ) to a velocity when to get the formula for the kinetic energy of an object of mass and velocity . Hint: Use a standard integration technique.    Kinetic energy of a spinning disk In order to find the kinetic energy of a spinning sphere (in the next problem), we first need to find the kinetic energy in a spinning disk. Suppose this disk has a radius of meters, a thickness , uniform density kg\/m , and is spinning at a rate of radians\/s around an axis through its center perpendicular to the disk. To find its total kinetic energy, we slice it into rings of radius and width .   (4 points) Find the mass, in kg, and velocity, in m\/s, of each ring. Use this and the formula you derived in problem 1 to find the kinetic energy of a ring.    (4 points) Use an integral to find the total kinetic energy of the disk.    Kinetic energy of the sphere (8 points) Now consider a sphere of radius m, uniform density kg\/m , spinning around an axis at radians\/s. By slicing the sphere into disks, find the kinetic energy of this sphere.  Baseball In the 2016 Major League season, the fastest pitch thrown was a 105.1 mph fastball by Aroldis Chapman. The spin rate on that pitch was 2617 rpm.   (3 points) Assuming a baseball is of uniform density (it isn't), what was the total kinetic energy of Chapman's pitch? (You will need to find out the size and mass of a baseball for this, and convert some units.)    (3 points) The highest recorded spin rate in the 2016 season was 3498 rpm on an 80 mph curveball by Seth Lugo. Did Lugo's spin rate make his pitch comparable to Chapman's in terms of kinetic energy?    Flipping! (10 points) Find the formula for the kinetic energy of a disk of radius and density , spinning end over end like a flipped coin.  "
+},
+{
+  "id": "Workshops-3-4",
+  "level": "2",
+  "url": "Workshops-3.html#Workshops-3-4",
+  "type": "Worksheet Exercise",
+  "number": "1.1.1",
+  "title": "The formula for kinetic energy.",
+  "body": "The formula for kinetic energy First, we will establish the following notation for this problem:   Velocity = = The rate of change in position.    Acceleration = = the rate of change in velocity.    Force equals mass acceleration     Work = Force distance when the force is constant.   The key to finding a formula for kinetic energy is to observe that the kinetic energy of an object of mass and velocity is the work done in accelerating that object to velocity from rest. Assuming that velocity is achieved when , the work integral to calculate kinetic energy is where the velocity is reached after seconds.   (3 points) Explain how the integral given above gives the work done in accelerating an object of mass $m$ to a velocity .    (5 points) Evaluate the integral given above to compute the work done in accelerating an object of mass from rest (when ) to a velocity when to get the formula for the kinetic energy of an object of mass and velocity . Hint: Use a standard integration technique.   "
+},
+{
+  "id": "Workshops-3-5",
+  "level": "2",
+  "url": "Workshops-3.html#Workshops-3-5",
+  "type": "Worksheet Exercise",
+  "number": "1.1.2",
+  "title": "Kinetic energy of a spinning disk.",
+  "body": "Kinetic energy of a spinning disk In order to find the kinetic energy of a spinning sphere (in the next problem), we first need to find the kinetic energy in a spinning disk. Suppose this disk has a radius of meters, a thickness , uniform density kg\/m , and is spinning at a rate of radians\/s around an axis through its center perpendicular to the disk. To find its total kinetic energy, we slice it into rings of radius and width .   (4 points) Find the mass, in kg, and velocity, in m\/s, of each ring. Use this and the formula you derived in problem 1 to find the kinetic energy of a ring.    (4 points) Use an integral to find the total kinetic energy of the disk.   "
+},
+{
+  "id": "Workshops-3-6",
+  "level": "2",
+  "url": "Workshops-3.html#Workshops-3-6",
+  "type": "Worksheet Exercise",
+  "number": "1.1.3",
+  "title": "Kinetic energy of the sphere.",
+  "body": "Kinetic energy of the sphere (8 points) Now consider a sphere of radius m, uniform density kg\/m , spinning around an axis at radians\/s. By slicing the sphere into disks, find the kinetic energy of this sphere. "
+},
+{
+  "id": "Workshops-3-7",
+  "level": "2",
+  "url": "Workshops-3.html#Workshops-3-7",
+  "type": "Worksheet Exercise",
+  "number": "1.1.4",
+  "title": "Baseball.",
+  "body": "Baseball In the 2016 Major League season, the fastest pitch thrown was a 105.1 mph fastball by Aroldis Chapman. The spin rate on that pitch was 2617 rpm.   (3 points) Assuming a baseball is of uniform density (it isn't), what was the total kinetic energy of Chapman's pitch? (You will need to find out the size and mass of a baseball for this, and convert some units.)    (3 points) The highest recorded spin rate in the 2016 season was 3498 rpm on an 80 mph curveball by Seth Lugo. Did Lugo's spin rate make his pitch comparable to Chapman's in terms of kinetic energy?   "
+},
+{
+  "id": "Workshops-3-8",
+  "level": "2",
+  "url": "Workshops-3.html#Workshops-3-8",
+  "type": "Worksheet Exercise",
+  "number": "1.1.5",
+  "title": "Flipping!",
+  "body": "Flipping! (10 points) Find the formula for the kinetic energy of a disk of radius and density , spinning end over end like a flipped coin. "
+},
+{
+  "id": "Workshops-4",
+  "level": "1",
+  "url": "Workshops-4.html",
+  "type": "Worksheet",
+  "number": "1.2",
+  "title": "Integrals with Computers",
+  "body": "Integrals with Computers  Even though we have learned integration techniques for many integrals in this class, it is often the case that antiderivatives cannot be written in terms of familiar functions (the so called elementary functions , which are essentially all the ones you learn about in Precalculus). When elementary antiderivatives don't exist (it's not just that you can't find them), one must turn to numerical approximation techniques to approximate definite integrals. We learned about these approximation techniques in class. They are very repetitive to do by hand, so we will program a computer to do them. In the process we will learn some Python using the Sympy and Numpy packages. This workshop is due Wednesday, February 22.    Some Matlab Commands and Code  Whenever you are using Matlab, you will want to have a .m file with the functions, code, and answers all in one place. When you have completed the problems on this workshop, you should print out your .m file and submit it with your answers. A file is posted on Canvas with an example to get you started.  One useful command in Matlab is help . If you get some output that you don't understand or are having trouble, you can type help followed by what you want help with and Matlab will attempt to give you some help.   Using an .m File  We will be using files with a .m extension as Matlab scripts. The Command Window is what you see if you just open Matlab on a computer. You can get answers to simple one step questions by working directly in the Command Window. However, for more involved problems that need a series of commands it is more useful to put them all in a script, then run them all in their correct sequence with one click. You do this with an .m file.  To get started, open the .m file posted on Canvas. After it has been opened, save it to somewhere you have access to it. You may want to rename it to something other than Riemannleft since you will modify it to do things other than a left-hand Riemann sum. Once it is saved, you can run the file by just clicking the Run button in the script window. If you get a pop up saying that the file is not in the current path, just click on change folder . Then the output will appear in the Command Window.    Entering a Symbolic Function.  Suppose you would like to define a function like in Matlab so that you just command Matlab to do things to it later in the script. To do this in your .m file, you would enter the following: syms x f(x) = sin(x^2)   The syms command declares the independent variable. Then you can do things like evaluate the function by just typing f(a) and hitting ’run’ to find the output for a given input . In this case it will just say sin(a^2) , if you want a decimal approximation you type double f(a) . For instance, suppose you want to compute for the function we defined above. You would type double(f(3)) and get an output of . If you want more decimal places, put the command format long at the start of your .m file to show more accuracy.    Doing Calculus Symbolic Functions.  You can do Calculus with a symbolic function using the following commands: diff(f(x),x) int(f(x),x) You can probably guess what those commands do, but try them to find out.    Using a For Loop.  For loops are a way to program a computer to perform a repetitive task a predetermined number of times. We discussed how a left-hand Riemann sum is really a For loop in class. Here we will discuss the .m file posted on Canvas for evaluating the left-hand Riemann sum approximation for using rectangles.    The first five lines of code should have fairly clear meaning.    In line six we must initialize the sum LS (for left sum ) to be zero. Each time it runs through the loop it will add on another term.    The for statement just has the limits you would have on the sum using notation.    To understand the evaluation, just note that and using the normal left Riemann sum notation.       Exercises.  For these exercises, we will be considering the definite integral     (2 points) Describe where you get stuck doing this integral by hand.    (2 points) What does Matlab give you for an antiderivative of (enter exp(x) for )? Is it useful (to you)?      (6 points) Use a graph of the derivative of to help you find so that the LEFT and RIGHT approximation of this integral is within of the exact value. Find this approximation. Include a printout of the graph of the derivative of in your write-up.    (6 points) Using the you found in (2a), what do the other methods give as approximations. What is the maximum possible error for each method of approximation? Include any relevant graphs to show how you arrived at your error bounds.    (6 points) For each of MID, TRAP, and SIMP, how large must $n$ be to make the approximation within of the exact value of this integral? Be sure to explain your reasoning.     "
+},
+{
+  "id": "Workshops-4-2",
+  "level": "2",
+  "url": "Workshops-4.html#Workshops-4-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "This workshop is due Wednesday, February 22. "
+},
+{
+  "id": "exercises-3",
+  "level": "2",
+  "url": "Workshops-4.html#exercises-3",
+  "type": "Worksheet Exercise",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  (2 points) Describe where you get stuck doing this integral by hand.    (2 points) What does Matlab give you for an antiderivative of (enter exp(x) for )? Is it useful (to you)?   "
+},
+{
+  "id": "exercises-4",
+  "level": "2",
+  "url": "Workshops-4.html#exercises-4",
+  "type": "Worksheet Exercise",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  (6 points) Use a graph of the derivative of to help you find so that the LEFT and RIGHT approximation of this integral is within of the exact value. Find this approximation. Include a printout of the graph of the derivative of in your write-up.    (6 points) Using the you found in (2a), what do the other methods give as approximations. What is the maximum possible error for each method of approximation? Include any relevant graphs to show how you arrived at your error bounds.    (6 points) For each of MID, TRAP, and SIMP, how large must $n$ be to make the approximation within of the exact value of this integral? Be sure to explain your reasoning.   "
+},
+{
+  "id": "Workshops-5",
+  "level": "1",
+  "url": "Workshops-5.html",
+  "type": "Worksheet",
+  "number": "1.3",
+  "title": "Worksheet",
+  "body": " Suppose you want to purchase a house that costs . The annual interest rate on your loan will be (so monthly), and the loan term will be 30 years and the monthly payment will be dollars. We want to solve for . Assume you make your first payment at the end of the first month you have possession of the house.  The goal of this problem is to write a repeating decimal as a fraction using a geometric series. We begin by recognizing a given geometric series as a repeating decimal.  "
+},
+{
+  "id": "Workshops-6",
+  "level": "1",
+  "url": "Workshops-6.html",
+  "type": "Worksheet",
+  "number": "1.4",
+  "title": "Worksheet",
+  "body": "  This workshop is due Monday, March 27. You may work in groups, but each student must turn in their own copy.  Consider the following student’s argument regarding the convergence of : Since for all and is a convergent -series, we can conclude that converges by the Comparison Test.  Consider the following student’s argument regarding the convergence of : Observe that if , then we have . Observe that is a positive continuous function and the improper integral diverges. Hence we can conclude that diverges by the Integral Test.  The main take away from the last two questions should be that you need to carefully check all the hypotheses of a convergence test in order to be sure its conclusion is correct. For each of the following series, carefully decide whether it converges or diverges, and if it converges, is the convergence absolute or conditional. By carfully , I mean you should do the following:    State which test you will use and say why.    Show all the hypotheses of the test are satisfied.    Perform any calculations necessary and give your conclusion.                      Hint: For near zero, .    "
+},
+{
+  "id": "Workshops-6-1",
+  "level": "2",
+  "url": "Workshops-6.html#Workshops-6-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "This workshop is due Monday, March 27. "
+},
+{
+  "id": "Workshops-7",
+  "level": "1",
+  "url": "Workshops-7.html",
+  "type": "Worksheet",
+  "number": "1.5",
+  "title": "Worksheet",
+  "body": "  This workshop is due Friday, April 7. Work in teams, and turn in one carefully written copy per team. It is strongly encouraged that each student write up their own copy even if it is not the copy that gets turned in.  The goal of this workshop is to connect the error bound for Taylor polynomial approximations to an error bound for a numerical integration method. Specifically, we will work through proving the error bound when for .  The main student learning goals for this assignment are the following:    You should become more comfortable with abstract mathematical notation and concepts.    You will better understand the connection between the midpoint rule and linear approximation.    You will become more accustomed to translating between verbal\/visual descriptions of mathematics and symbolic manipulation. This is a critical communication skill in STEM disciplines.    Before we begin the questions, we first need to set up the notation for all of the questions. We will have a function that is defined and twice differentiable for . We suppose that for all . The interval is subdivided into intervals of equal length . The endpoints of each sub-interval are labeled as follows: The midpoints of each sub-interval are given by .   In terms of what are the quantities , , and . Show your answers algebraically and explain with a picture.  Now we’ll put it together.  "
+},
+{
+  "id": "Workshops-7-1",
+  "level": "2",
+  "url": "Workshops-7.html#Workshops-7-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "This workshop is due Friday, April 7. "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
